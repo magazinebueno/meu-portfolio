3cd39be3c52a4a4cbcb10775b976184ac1f12487
magazinebueno <35349266+magazinebueno@users.noreply.github.com>
2026-03-06 16:33:30 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: '/siasiteportfolio',
-  assetPrefix: '/siasiteportfolio',
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
